0ad4eae3771a6d293b622c9d5ae12931efd6ae0b
pranay-yalla <pranayyalla06@gmail.com>
2026-02-13 16:30:24 +0530

    Add string reversal methods and print results

--- stringreverse.java
+++ stringreverse.java
@@ -3,6 +3,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		String  text=sc.nextLine();
+		String  rev=reverse(text);
+		System.out.println(rev);
+		String  revwithsb=reversewithsb(text);
+		System.out.println(revwithsb);
+	}
+	public static String reverse(String text){
 		char[] a=text.toCharArray();
 		for(int i=0,j=a.length-1;i<j;i++,j--)
 		{
@@ -10,9 +16,10 @@ public class Main {
 			a[i]=a[j];
 			a[j]=temp;
 		}
-	String 	tex=new String(a);
-		System.out.println(tex);
-		
+	return new String(a);
+	
+	}
+	public static String reversewithsb(String text){
 		
 		StringBuilder sb=new StringBuilder();
 	//	sb.reverse(); direct method to.reverse
@@ -20,6 +27,6 @@ public class Main {
 	{
 		sb.append(text.charAt(i));
 	}
-		System.out.print(sb.toString());
+		return sb.toString();
 	}
 }
