e962f07a1f186e479857b9f60de4a9813f006b67
pranay-yalla <pranayyalla06@gmail.com>
2026-02-14 21:36:35 +0530

    Reverse words in input sentence

--- reverse-words.java
+++ reverse-words.java
@@ -2,10 +2,10 @@ import java. util. *;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner (System.in);
-		String s=sc.nextLine(). trim();
-		String[]  a=s.split("\\s+ ");
-		for(int i=a.length-1;i>=0;i--)
+		String sentence=sc.nextLine(). trim();
+		String[]  words=sentence.split("\\s+ ");
+		for(int i=words.length-1;i>=0;i--)
 		System.out.print(a[i]+" ");
-		
+		sc.close() ;
 	}
 }
